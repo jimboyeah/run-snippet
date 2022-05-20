0e31e82551c960a345158f649455afaa86c21f1b
Jeangowhy <jimboyeah@gmail.com>
2022-05-21 05:41:06 +0800

    fix to reload RSettings

--- Settings.py
+++ Settings.py
@@ -3,6 +3,12 @@ import sublime_api as sapi
 from sublime import *
 from sublime_plugin import *
 
+'''
+- User/RunSnippet.sublime-settings
+- RunSnippet/material/bash 5.1.md
+- RunSnippet/material/linux_cli_script_bible.md
+'''
+
 _Default = {
     "_filename": "RunSnippet.sublime-settings",
     "jump_between_group": True,
@@ -18,24 +24,38 @@ def init(target, default):
             continue
         target.set(key, default[key])
 
+def load():
+    global RSettings, _Default
+    RSettings = load_settings(_Default['_filename'])
+    return RSettings
+
+def save():
+    global RSettings, _Default
+    save_settings(_Default['_filename'])
+
 def get(key):
     global RSettings
+    if not RSettings:
+        load()
+        print("RSettings reload:", RSettings)
     return RSettings.get(key)
 
 def on_change():
     global RSettings, _Default
-    RSettings = load_settings(_Default['_filename'])
+    load()
     print("settings on_change")
     print("Load", _Default['_filename'], RSettings)
     if RSettings:
         print("  ==> jump_between_group", RSettings.get("jump_between_group"))
 
 # Load settings from /Packages/Users
-RSettings = load_settings(_Default['_filename'])
+# RSettings = load_settings(_Default['_filename'])
+load()
 RSettings.add_on_change('setting', on_change)
 init(RSettings, _Default)
 # Save settings to /Packages/Users
-RSettings = save_settings(_Default['_filename'])
+# RSettings = save_settings(_Default['_filename'])
+save()
 
 
 # class RSettings(Settings):
