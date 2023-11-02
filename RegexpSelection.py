6037cb72683fda02aac5dc4fe805e75285bd3e90
Jeangowhy <254141203@qq.com>
2023-11-03 01:46:04 +0800

    history (remember the last)

--- RegexpSelection.py
+++ RegexpSelection.py
@@ -5,8 +5,15 @@ from sublime_plugin import *
 
 class RegexpSelection(sp.WindowCommand):
 
+    # <?(\w+\.){0,}(\w+\.?)?<[A-Z\<\>,\?]+>
+    # Type Hint cause error under Python 3.8: TypeError 'type' object is not subscriptable
+    # def initial_selection(self) -> list[tuple[int, int]]:
+    # history: list[str] = list()
+    history = list()
+
     def run(self, regexp:str):
         print("regexp_selection run:", regexp, self.window)
+        self.history.append(regexp)
         (res, regions) = RegexpSelection.find_all(regexp)
         view = self.window.active_view()
         if view:
@@ -25,7 +32,7 @@ class RegexpSelection(sp.WindowCommand):
 
     def input(self, args):
         print("regexp_selection input:", args)
-        return SimpleInputHandler()
+        return SimpleInputHandler(self.history[-1] if len(self.history) else "")
 
     def validate(self, text, event):
         print("regexp_selection validate:", event)
@@ -52,11 +59,23 @@ class RegexpSelection(sp.WindowCommand):
 
 class SimpleInputHandler(sp.TextInputHandler):
 
+    def __init__(self, default:str) -> None:
+        super().__init__()
+        self.default = default
+
     def name(self): # args name to transport data in command.run(...)
         return "regexp"
 
     def placeholder(self): # a text show as backgroud of input box in GUI
         return "Text as RegexpSelection" 
+
+    def initial_text(self):
+        return self.default
+
+    def initial_selection(self) -> list:
+        sel: list[tuple[int, int]] = list()
+        sel.append( (int(0), len(self.default)) )
+        return sel
 
     def preview(self, text): # return some text/html preview on GUI
         if text is None or text == "":
