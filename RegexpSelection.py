97fdb8f4de9ef30f15ef79309c30985390a30058
Jeangowhy <254141203@qq.com>
2023-11-03 07:06:52 +0800

    =====================   =============================     1. Java Generics:      (\w+\.)*(\w+\.?)?<[A-z<> ,?]+?>     2. reStructuredText     3. Section Title:      \n\n(?=[-+=#~.`'"^*]{3, })([-+=#~.`'"^*]+)\n.+\n\1     4. Subtitle:           \n\n(?![-+=#~.`'"^*]{3, }).+\n(?=[-+=#~.`'"^*]{3,}).+     5. paragraphs begin:   \n\n(?=[^-+=#~.'"^*]{3, })[^ ]+.+\n(?![-+=#~.'"^*]{3, })     =====================   =============================

--- RegexpSelection.py
+++ RegexpSelection.py
@@ -7,11 +7,6 @@ from typing import Optional, TYPE_CHECKING
 
 class RegexpSelection(sp.WindowCommand):
 
-    # Java Generics: (\w+\.)*(\w+\.?)?<[A-z<> ,?]+?>
-    # reStructuredText 
-    # Section Title: \n\n(?=[-+=#~.`'"^*]{3, })([-+=#~.`'"^*]+)\n.+\n\1
-    # Subtitle: \n\n(?![-+=#~.`'"^*]{3, }).+\n(?=[-+=#~.`'"^*]{3,}).+
-    # paragraphs begin: \n\n(?=[^-+=#~.'"^*]{3, })[^ ]+.+\n(?![-+=#~.'"^*]{3, })
     # Type Hint cause error under Python 3.8: TypeError 'type' object is not subscriptable
     # def initial_selection(self) -> list[tuple[int, int]]:
     # history: list[str] = list()
@@ -41,7 +36,7 @@ class RegexpSelection(sp.WindowCommand):
 
     def input(self, args):
         print("regexp_selection input:", args)
-        if args.get('history') is None:
+        if args.get('history') is None and args.get('regexp') is None:
             return SimpleInputHandler(self.history[-1] if len(self.history) else "")
 
     def validate(self, text, event):
